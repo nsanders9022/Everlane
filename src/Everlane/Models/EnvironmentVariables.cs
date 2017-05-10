5e8b8c1926076b1a6f53bb45bf469d2e45d0de62
Cassie Musolf, Nicole Sanders, Shruti Priya and Tammy Dang <cassie+nicole+shruti+tammy@>
2017-05-10 10:26:11 -0700

    include Yandex API and add environmentVariables file with key.

---
+++ src/Everlane/Models/EnvironmentVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Everlane.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string YandaxKey = "trnsl.1.1.20170510T165628Z.21b584f2009fc3b4.1d4d6367957a3e38215789b338f78a5d54c42ecb";
+    }
+
+    //According to the Terms of Use for the Yandex.Translate service, the text “Powered by Yandex.Translate” must be shown above or below the translation result, with a clickable link to the page http://translate.yandex.com/.
+}
+
+
+//https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170510T165628Z.21b584f2009fc3b4.1d4d6367957a3e38215789b338f78a5d54c42ecb&text=i%love%chocolate&lang=de&[format=plain]&[options=1]
---
+++ src/Everlane/Models/Translator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace Everlane.Models
+{
+    public class Translator
+    {
+        public string To { get; set; }
+        
+        public string Body { get; set; }
+
+        public static  string GetTranslation()
+        {
+            var client = new RestClient("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170510T165628Z.21b584f2009fc3b4.1d4d6367957a3e38215789b338f78a5d54c42ecb&text=i%love%chocolate&lang=de&[format=plain]&[options=1]");
+            var request = new RestRequest("", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var translation = JsonConvert.DeserializeObject<string>(jsonResponse["text"].ToString());
+            return translation;
+
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
